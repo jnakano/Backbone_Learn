2ffbd913e6bfbe0704738f8c8b33bacfac848d9c
Jonhnson Nakano <jnakano89@gmail.com>
2012-09-12 17:25:32 -0700

    added sample.js

--- app.js
+++ app.js
@@ -7,25 +7,29 @@ $(document).ready(function() {
 //		el: $("#abc"),
 
 		setName: function(text) {
+			this.name = text;
 			
 		
 		},
 		
 		render:function(){
-			this.$el.html("<a href='http://www.google.com'>hello world! </a>");
+			this.$el.html("<a href='http://www.google.com'>" + this.name + "</a>" + "<br /> <br />");
 			return this;
 		
 		}
 	
 	});
 	
-	var app1 = new AppView();	
+	var app1 = new AppView();
+	app1.setName("N1");	// 
 	$(document.body).append(app1.render().el) //AppView().el
 
 	var app2 = new AppView();	
+	app2.setName(" Hello2 ")
 	$(document.body).append(app2.render().el) //AppView().el
 	
-	var app3 = new AppView();	
+	var app3 = new AppView();
+	app3.setName(" Hello 3 ");	
 	$(document.body).append(app3.render().el) //AppView().el
 	
 	
---
+++ sample.js
@@ -0,0 +1,3 @@
+var a = "anything";
+var b = "string2";
+var c = "another" + a + "whatever" + b;
